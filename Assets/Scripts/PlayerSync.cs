7b9fcb052df981f02dd82f9d9d4c78b4acb53dd8
Joshua Favre <jfavre390@gmail.com>
2020-07-27 12:31:18 -0400

    Lobby screen first pass

--- Assets/Scripts/PlayerSync.cs
+++ Assets/Scripts/PlayerSync.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using Photon.Pun;
+using UnityEditor.Experimental.TerrainAPI;
 
 public class PlayerSync : MonoBehaviourPun, IPunObservable
 {
---
+++ Assets/Scripts/RoomUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class RoomUI : MonoBehaviour
+{
+	[SerializeField] TMP_Text roomNameLabel = null;
+	[SerializeField] TMP_Text playerCountLabel = null;
+
+	[Space]
+	[SerializeField] string roomName = "";
+	[SerializeField] int currentPlayers = 0;
+	[SerializeField] int maxPlayers = 0;
+
+	//TODO Error checking
+	public string RoomName { get { return roomName; } set { roomName = value; UpdateLabels(); } }
+	public int CurrentPlayers { get { return currentPlayers; }  set { currentPlayers = value; UpdateLabels(); } }
+	public int MaxPlayers { get { return maxPlayers; } set { maxPlayers = value; UpdateLabels(); } }
+
+	public void SetLabels(string roomName, int maxPlayers, int currentPlayers = 0)
+	{
+		this.roomName = roomName;
+		this.currentPlayers = currentPlayers;
+		this.maxPlayers = maxPlayers;
+		UpdateLabels();
+	}
+
+	private void OnValidate() => UpdateLabels();
+	private void UpdateLabels()
+	{
+		roomNameLabel.text = roomName;
+		playerCountLabel.text = $"{currentPlayers}/{maxPlayers}";
+	}
+}
