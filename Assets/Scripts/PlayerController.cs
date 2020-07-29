734d138c7ecd4cd4b5a48c10cd6a0c2f03dc3e72
Alexjandropiedra <alexrock9657@gmail.com>
2020-07-29 10:00:12 -0600

    Player Pushing

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,4 +9,16 @@ public class PlayerController : MonoBehaviour
 		transform.Translate(Input.GetAxis("Vertical") * transform.forward * m_speed * Time.deltaTime);
 		transform.Translate(Input.GetAxis("Horizontal") * transform.right * m_speed * Time.deltaTime);
 	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		float force = 500;
+
+		if (collision.gameObject.tag == "Player")
+		{
+			Vector3 dir = collision.contacts[0].point - transform.position;
+			dir = -dir.normalized;
+			collision.gameObject.GetComponent<Rigidbody>().AddForce(-dir * force);
+		}
+	}
 }
